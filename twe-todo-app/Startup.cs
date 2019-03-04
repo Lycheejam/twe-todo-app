fdbcb58c0379af6bdea64b3ffe70d5790da9901d
Lycheejam <lycheejamwow@gmail.com>
2019-03-05 02:37:45 +0900

    fix consumer key and add secret.json

--- twe-todo-app/Startup.cs
+++ twe-todo-app/Startup.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using twe_todo_app.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Security.Claims;
-using System.Threading;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using System.Net;
-using Microsoft.AspNetCore.Authentication.Twitter;
+using twe_todo_app.Data;
 
 namespace twe_todo_app {
     public class Startup {
